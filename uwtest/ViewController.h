0a79ddee3ddedc8d2cb8bc30870b1892ad9de274
Daniel Leping <dileping@gmail.com>
2015-09-25 18:16:45 +0300

    Initial check in

---
+++ uwtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uwtest
+//
+//  Created by Daniel Leping on 9/25/15.
+//  Copyright © 2015 Crossroad Labs, LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ uwtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  uwtest
+//
+//  Created by Daniel Leping on 9/25/15.
+//  Copyright © 2015 Crossroad Labs, LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
